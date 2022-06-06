ea6ff6b4a2f1b78885763998de992784e341333c
gabriel gonçalves <gabriel24f90@gmail.com>
2022-06-06 09:30:11 -0300

    PROVA

--- api/src/repository/conection.js
+++ api/src/repository/conection.js
@@ -1,9 +1,9 @@
 import MYSQL from 'mysql2/promise'
-const con = await MYSQL.createConnection(){
-    host:PROCESS.env.MYSQL_HOST,
-    user:PROCESS.env.mysql_user,
-    password:process.env.mysql_pwb
-    databse:process.env.mysql_db
 
-
-}
+const con = await MYSQL.createConnection({
+    host:process.env.MYSQL_HOST,
+    user:process.env.MYSQL_user,
+    password:process.env.MYSQL_pwb,
+    databse:process.env.MYSQL_db
+})
+export {con}
---
+++ api/src/repository/petrepository.js
@@ -0,0 +1,18 @@
+import {con} from './conection.js'
+
+export async function inserirpet(pet){
+    const comando=
+    `INSERT INTO id_pet(id_pet, nm_pet)
+    VALUES(?,?)`
+    const [resp = await con.query(comando, pet.nome,)]
+    return pet;
+}
+
+export async function listarpet(id){
+    const comando=
+    'SELECT id_pet      id'
+        'nm_pet         nome'
+        'from tb_pet'
+        const [ linhas] = await con.query
+        return pet;
+}
